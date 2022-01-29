e8517df5f2e12bb30744d00565137a0ec7c53ace
Elvis <elvisjulius@hotmail.com>
2022-01-29 16:28:55 -0300

    adicionando customer controller

--- src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
+++ src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -1,14 +1,25 @@
 package com.mercadolivro.controller
 
+import com.mercadolivro.controller.request.PostCostumerRequest
+import com.mercadolivro.model.CustomerModel
+import org.springframework.expression.spel.ast.Elvis
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("customer")
 class CustomerController {
 
     @GetMapping
-    fun helloWorld(): String{
-        return "Hello World"
+    fun getCustomers(): CustomerModel{
+        return CustomerModel("1", "Elvis","elvisjulius@hotmail.com")
     }
 
+    @PostMapping
+    fun create(@RequestBody customer: PostCostumerRequest){
+        println(customer)
+    }
 }
