a3b9a119059b1dd280aafc93de06c4dd75b62860
Elvis <elvisjulius@hotmail.com>
2022-01-29 17:22:25 -0300

    adicionando um path variable

--- src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
+++ src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -3,23 +3,41 @@ package com.mercadolivro.controller
 import com.mercadolivro.controller.request.PostCostumerRequest
 import com.mercadolivro.model.CustomerModel
 import org.springframework.expression.spel.ast.Elvis
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("customer")
 class CustomerController {
 
+    val customers = mutableListOf<CustomerModel>()
+
     @GetMapping
-    fun getCustomers(): CustomerModel{
-        return CustomerModel("1", "Elvis","elvisjulius@hotmail.com")
+    fun getAll(): MutableList<CustomerModel> {
+        return customers
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     fun create(@RequestBody customer: PostCostumerRequest){
-        println(customer)
+
+        val id = if(customers.isEmpty()){
+            "1"
+        } else {
+            customers.last().id.toInt() + 1
+        }.toString()
+
+        customers.add(CustomerModel(id, customer.name, customer.email))
+    }
+
+    @GetMapping("/{id}")
+    fun getCostumer(@PathVariable id: String): CustomerModel {
+        return customers.filter { it.id == id }.first()
     }
 }
